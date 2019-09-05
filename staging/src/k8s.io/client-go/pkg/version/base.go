e4af4bd9c76d97da4a1404011df57583e0b6f14c
Darren Shepherd <darren@rancher.com>
2019-09-05 10:58:46 -0700

    v1.15.3-k3s.3

--- staging/src/k8s.io/client-go/pkg/version/base.go
+++ staging/src/k8s.io/client-go/pkg/version/base.go
@@ -4,7 +4,7 @@ var (
 	gitMajor = "1"
 	gitMinor = "15"
 	gitVersion   = "v1.15.3-k3s.3"
-	gitCommit    = "5d32fe29850d8163bd41c5b3dd5f66df9542a8ff"
+	gitCommit    = "1844154d8e4c91325ed1041387bd7e326fa7b546"
 	gitTreeState = "clean"
-	buildDate = "2019-09-05T17:54+00:00Z"
+	buildDate = "2019-09-05T17:58+00:00Z"
 )
