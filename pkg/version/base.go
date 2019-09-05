1844154d8e4c91325ed1041387bd7e326fa7b546
Darren Shepherd <darren@rancher.com>
2019-09-05 10:54:46 -0700

    v1.15.3-k3s.3

--- pkg/version/base.go
+++ pkg/version/base.go
@@ -4,7 +4,7 @@ var (
 	gitMajor = "1"
 	gitMinor = "15"
 	gitVersion   = "v1.15.3-k3s.3"
-	gitCommit    = "19bf9feead9fcbb6d76792cdf172e6f2e22c75ff"
+	gitCommit    = "5d32fe29850d8163bd41c5b3dd5f66df9542a8ff"
 	gitTreeState = "clean"
-	buildDate = "2019-09-05T17:33+00:00Z"
+	buildDate = "2019-09-05T17:54+00:00Z"
 )
